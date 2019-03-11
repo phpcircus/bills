481df539865c17e4e4da82b077683e8395f550ed
Clayton Stone <devcircus@outlook.com>
2019-03-11 14:19:57 -0500

    Force https on all routes.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if ('production' === config('app.env')) {
+            URL::forceScheme('https');
+        }
     }
 }
